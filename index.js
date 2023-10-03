baef5adecf6c7f7370ef4813c618d3edae0b953c
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-10-03 17:08:47 +0530

    create amdmin model and schema

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const { Mongoose } = require('mongoose')
 require('dotenv').config()
 
 // Middlewares
 app.use(cors())
 app.use(bodyParser.json())
 
+// Db  Connect
+const connectDb = async()=> {
+  try {
+   const res =  await mongoose.connect(process.env.MONGOURL);
+   if(res){
+     console.log('Db connected')
+   }
+  } catch (error) {
+    console.error(error);
+  }
+}
+connectDb()
+
+// Models
+const adminSchema = new mongoose.Schema({
+  email: String,
+  password: String
+})
+const Admin = mongoose.model('Admin', adminSchema)
+
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/api/admin/signup', (req, res) => {
+  const body = req.body
+  console.log(body)
+  res.send(body)
 })
 
 
